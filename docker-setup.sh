d1acae3258ef3dedca72cda0c113691d4bd734af
Miguel Reis <miguel_reis08@protonmail.com>
2024-01-21 16:57:27 -0300

    Update docker-setup.sh script

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y curl vim
-curl -fsSL https://get.docker.com | bash
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+
+# Install Docker
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
